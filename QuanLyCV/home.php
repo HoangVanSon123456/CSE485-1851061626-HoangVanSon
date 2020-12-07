3a156d6aed53040740186a1f5bc8229b20f84e6d
HoangVanSon123456 <63546473+HoangVanSon123456@users.noreply.github.com>
2020-12-07 19:59:40 +0700

    QuanLyCv

---
+++ QuanLyCV/home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;1,300&display=swap"
+        rel="stylesheet">
+    <title>Home</title>
+</head>
+<body>
+     <div class="header">
+        <nav>
+            <div class="row">
+                <img src="resources/img/logo-white.png" alt="OmniFood logo" class="logo">
+                <ul class="main-nav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Me</a></li>
+                    <li><a href="#">Resume</a></li>
+                    <li><a href="#">Works</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="hero-text-box">
+            <h1>I'M Professional<br> Web Developer</h1>
+            <a class="bnt btn-full" href="#">DOWNLOAD RESUME</a>
+            <a class="bnt btn-ghost" href="#">HIRE ME</a>
+        </div>
+     </div>
+</body>
+</html>
