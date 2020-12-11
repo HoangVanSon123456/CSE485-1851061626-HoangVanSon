e6aee383027b2d87e28ba12dba4815b2fd477d54
Tatu1602 <18510616112@e.tlu.edu.vn>
2020-12-11 21:48:50 +0700

    web cv

---
+++ Web CV/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resources/style.css">
+    <title>Curriculum Vitae </title>
+</head>
+<body>
+    <nav class="navbar">
+        <div class="row">
+            <div class="logo"><a href="#"><span>C</span>urriculum<span>V</span>itae</a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#resume" class="menu-btn">Resume</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+    <section class="home" id="home">
+        <div class="row">
+            <div class="home-content">
+                <div class="text-1">Hello, my name is</div>
+                <div class="text-2">Ta Anh Tu</div>
+                <div class="text-3">And I'm a <span class="typing">Youtuber</span></div>
+            </div>
+        </div> 
+    </section>
+    
+</body>
+</html>
