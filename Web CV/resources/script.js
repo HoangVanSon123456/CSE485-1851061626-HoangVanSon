81e4ad6dea4d41a7cceb0096640ef96a43758744
HoangVanSon123456 <63546473+HoangVanSon123456@users.noreply.github.com>
2021-01-04 20:11:05 +0700

    @@

--- Web CV/resources/script.js
+++ Web CV/resources/script.js
@@ -16,5 +16,26 @@ $(document).ready(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+    //owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autophayTimeOut: 2000,
+        autophayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    })
 
 });
