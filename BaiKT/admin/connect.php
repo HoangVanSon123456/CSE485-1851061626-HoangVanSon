ef6d0ef4e15fe0409edc83c4cd2e44d62cdc04cb
HoangVanSon123456 <63546473+HoangVanSon123456@users.noreply.github.com>
2021-01-16 16:17:43 +0700

    xóa file không cần

--- BaiKT/admin/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = mysqli_connect('localhost','root','','baikt');
-    /*db_host,db_user,db_password,db_name*/ 
-    if(!$conn){
-        die('Co loi ket noi'.mysqli_connect_error());
-    }
-?>
